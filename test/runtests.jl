7c37ee0e4e301efc13239c052109c8c2e3fc3e6d
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-06-06 14:00:26 +0200

    Added MockMap and TestMap

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,11 @@ using Test
 
 include("CellValuesMocks.jl")
 
+include("MapsMocks.jl")
+
 @testset "CellValuesMocks" begin include("CellValuesMocksTests.jl") end
+
+@testset "MapsMocks" begin include("MapsMocksTests.jl") end
 
 @testset "Gallery" begin include("GalleryTests.jl") end
 
