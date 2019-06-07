59000efe9d70b88db8719e2cd0288ec3557e62fa
fverdugo <fverdugo@cimne.upc.edu>
2019-06-07 07:34:56 +0200

    Updating reference to TensorValues

--- src/CellwiseValues.jl
+++ src/CellwiseValues.jl
@@ -2,19 +2,6 @@ module CellwiseValues
 
 using Reexport
 
-# To be ported to TensorValues
-using TensorValues
-import Base: sum
-import Base: +, -, *, /
-sum(a::MultiValue) = sum(a.array)
-for op in (:+,:-,:*)
-  @eval begin
-    ($op)(a::MultiValue,b::Number) = MultiValue($op(a.array,b))
-    ($op)(a::Number,b::MultiValue) = MultiValue($op(a,b.array))
-  end
-end
-(/)(a::MultiValue,b::Number) = MultiValue(a.array/b)
-
 include("Helpers.jl")
 @reexport using CellwiseValues.Helpers
 
